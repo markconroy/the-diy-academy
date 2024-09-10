76f373d018fffaff7b0250fe152412fe55db3326
Mark Conroy <mark@mark.ie>
2024-09-10 12:43:00 +0100

    adds new wait list for 2025 course

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log('Hello from The DIY Academy!');
+
+const dialog = document.querySelector(".dialog");
+const dialogCloseButton = document.querySelector(".dialog__close button");
+const dialogContent = document.querySelector(".dialog__content");
+const waitingListButtons = document.querySelectorAll(
+  ".buy-now-button--waiting-list"
+);
+
+waitingListButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    dialogContent.innerHTML = `
+      <div id="fd-form-66e02649f4725e8e9c24793d"></div>
+    `;
+    let script = document.createElement('script');
+    script.textContent = `
+      window.fd('form', {
+        formId: '66e02649f4725e8e9c24793d',
+        containerEl: '#fd-form-66e02649f4725e8e9c24793d'
+      });
+    `;
+    dialogContent.appendChild(script);
+    dialog.showModal();
+  });
+});
+
+dialogCloseButton.addEventListener("click", () => {
+  dialog.close();
+});
